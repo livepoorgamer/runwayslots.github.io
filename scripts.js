7127285e7d4d5cee9b5fb95a931b3de82952b520
livepoorgamer <livepoorgamer@gmail.com>
2024-12-21 11:19:39 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -38,6 +38,23 @@ function createForm(action) {
     formSection.style.display = 'block'; // Show the form
 }
 
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
+
+// Show or Hide the Back to Top Button
+window.onscroll = function () {
+    const backToTopButton = document.getElementById('backToTop');
+    if (document.documentElement.scrollTop > 200 || document.body.scrollTop > 200) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+}
+
 // Validate seats input length
 function validateSeats(input) {
     if (input.value.length > 3) {
